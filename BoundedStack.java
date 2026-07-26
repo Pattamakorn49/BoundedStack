5b73535d138ecf78c5372dbec5b00a2cb3ceb476
Pattamakorn49 <pattamakorn.j@ku.th>
2026-07-26 21:49:58 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -1,14 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 
  * BoundedStack - ADT คือ ระบบการจัดเก็บหนังสือ
  * From 6821601186 ปัตมากร เจริญใจ และ 6821601429 วัลย์วลี ยุติธรรม
  * 
- * ค่านามธรรม (A) : ลำดับการจัดเก็บหนังสือ เช่น [BOOK1 , BOOK2 , BOOK3 ,..., BOOK100]
 */
 public class BoundedStack {
 
@@ -20,29 +16,28 @@ public class BoundedStack {
     private final int capacity;
 
     //Abstaction Function(AF) : (book,capacity)
+    // book คือ 
+    // capacity คือ 
     
     //Representation Invariant(RI) :
-    // - book ต้องไม่เป็น null
-    // - book ต้องมีรายการประวัติจริง
+    // - book ต้องไม่เป็น null 
+    // - book ต้องมีรายการประวัติจริง 
     // - แต่ละรายการต้องไม่เป็น null
     // - แต่ละรายการต้องไม่เป็น String ว่าง
-    // - จำนวนประวัติต้องไม่เกิน MAX_BOOKS
-    // - อนุญาตให้มีประวัติการตำนวณซ้ำกัน
+    // - จำนวนจัดเก็บต้องไม่เกิน MAX_BOOKS
+    // - ชื่อหนังสือต้องไม่ซํ้ากัน
 
-    // Safety from rep exposre :
-    // ...
-
-    /** 
-     * เขียน checkRep()
-     * แปลง RI ทุกข้อเป็น assert หนึ่งบรรทัด พร้อมข้อความอธิบาย
-     */
 
     private void checkRep() {
         assert book != null : "history is not null";
         assert book.size() <= MAX_BOOKS : "history exceeds maximum size";
+        Set<String> seen = new HashSet<>();
         for(String b : book) {
             assert b != null : "history must not be null";
             assert !b.isEmpty() : "history most not be empty";
+            assert b!="";
+            assert seen.add(b):"ชื่อเพลงซ้ำ: "+b;
+            
         }    
     }
 
@@ -69,8 +64,14 @@ public class BoundedStack {
     */
 
    public BoundedStack(List<String> s) {
-        this.book = null;
+    if(s==null||s.size()>MAX_BOOKS)throw new IllegalArgumentException();
+    Set<String> seen = new HashSet<>();
+    for(String b : s){
+         if(b==null||b=="") throw new IllegalArgumentException();
+            if(!seen.add(b)) throw new IllegalArgumentException();}
+        this.book = new ArrayList<>(s);
         this.capacity = 0;
+        checkRep();
    }
 
    // ==== Mutators ====
@@ -78,23 +79,29 @@ public class BoundedStack {
    /**
     * เพิ่มหนังสือต่อท้ายระบบจัดเก็บ
     * 
-    * @param s
-    * @return 
+    * @param s หนังสือต้องไม่เป็น null และไม่เป็นสตริงว่าง
     * @return true ถ้าเพิ่มสำเร็จ , false ถ้ามีหนังสือเล่มนี้อยู่แล้วหรือเต็มแล้ว
-    * @throws IllegalArgumentException ถ้า book เป็น null หรือสตริงว่าง 
+    * @throws IllegalArgumentException ถ้า s เป็น null หรือสตริงว่าง 
     */
    public boolean push(String s){
-        return false;
+    if(s==null||s =="")throw new IllegalArgumentException();
+    if(book.contains(s)||book.size()>=MAX_BOOKS)return false; 
+        book.add(s);
+        checkRep();
+        return true;
    }
 
    /**
     * ลบหนังสือออกจากระบบจัดเก็บ
     * 
-    * @param book ชื่อหนังสือที่ต้องการลบ
+    * @param s ชื่อหนังสือที่ต้องการลบ
     * @return true ถ้าลบสำเร็จ , false ถ้าไม่พบชื่อหนังสือเล่มนี้
     */
-   public boolean remove(String book) {
-        return false;
+   public boolean remove(String s) {
+    if(!book.contains(s))return false;
+       book.remove(s);
+       checkRep();
+       return true;
    }
 
    // ==== Observers ====
@@ -103,14 +110,14 @@ public class BoundedStack {
     * คืนจำนวนหนังสือในระบบจัดเก็บ
     */
    public int size() {
-        return -1;
+        return book.size();
    }
 
    /** 
     *  ตรวจว่ามีหนังสือเล่มนี้อยู่หรือไม่
     */
-   public boolean contains(String book) {
-        return false;
+   public boolean contains(String s) {
+        return book.contains(s);
    }
 
    /**
@@ -119,7 +126,7 @@ public class BoundedStack {
     * ระวัง : ห้ามมคืน reference ของ book ตรงๆ (rep exposure!)
     */
    public List<String> book() {
-        return null;
+        return new ArrayList<>(book);
    }
 
    // ==== Producer ====
@@ -130,7 +137,9 @@ public class BoundedStack {
     * @return ระบบจัดเก็บหนังสือใหม่ที่สลับลำดับแล้ว
     */
    public BoundedStack shuffled() {
-    return null;
+        List<String> copy = new ArrayList<>(book);
+        Collections.shuffle(copy);
+        return new BoundedStack(copy);
    }
 
    @Override
